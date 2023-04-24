0fe01fcb147464ddd58c0be93f39d476d4c8a8b2
nsamedaisy <nsamedaisy@gmail.com>
2023-04-24 12:24:23 +0100

    fixed problem

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const msg1 = document.getElementById("message1")
 const msg2 = document.getElementById("message2") 
 const msg3 = document.getElementById("message3") 
 const answer = Math.floor(Math.random() * 100) + 1;
-const no_of_guesses = 0;
+let no_of_guesses = 0;
+let numguess = 7;
 const guesses_num = [];
 
 function play() {
   const user_guess = document.getElementById("guess").value;
     if (user_guess < 1 || user_guess > 100) {
     alert("Please Enter a number Between 1 to 100");
-    } else {
+    } else if(numguess!=0){
     guesses_num.push(user_guess);
     no_of_guesses += 1;
     if (user_guess < answer) {
@@ -24,11 +25,15 @@ function play() {
       msg1.textContent = "Your Guess is Too High"
       msg2.textContent = "No. Of Guesses : " + no_of_guesses;
       msg3.textContent = "Guessed Number Are: " + guesses_num;
-    } else if (user_guess == answer) {
+    } else if (user_guess === answer) {
       msg1.textContent = "CONGRATULATIONS!!! you won"
       msg2.textContent = "the Number was " + answer 
       msg3.textContent = " You guessd it in " + no_of_guesses + "Guesses";
     }
+   numguess --;
+   if(numguess === 0){
+    msg1.textContent = `Game over! Number was ${answer}`
+   }
   }
 }
 
@@ -40,14 +45,14 @@ function play() {
 // const numguess = 1;
 
 // if(numguess === 7){
-	// 	displayguess(user_guess);
-	// 	displayMessage(`Game over! Number was ${answer}`);
-	// 	end-Game();
-	// }
-	// else{
-	// 	displayguess(user_guess);
-	// 	checkguess(user_guess);
-	// }
+// 		displayguess(user_guess);
+// 		displayMessage(`Game over! Number was ${answer}`);
+// 		end-Game();
+// 	}
+// 	else{
+// 		displayguess(user_guess);
+// 		checkguess(user_guess);
+// 	}
 
 
 
